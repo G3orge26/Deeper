b9c9cda52cce87513e6850df8962ba2ca1f46e70
gkaliv <g3orge26@hotmail.com>
2022-05-31 16:52:27 +0300

    Refined Inventory UI

--- Scenes/UI/UI.gd
+++ Scenes/UI/UI.gd
@@ -13,14 +13,14 @@ func _ready():
 	pass
 
 func update_health():
-	$PanelContainer/VBoxContainer/HP_bar.max_value = player_obj.MAX_HP
-	$PanelContainer/VBoxContainer/HP_bar.value = player_obj.HP
-	$PanelContainer/VBoxContainer/HP_bar/Label.text = str(player_obj.HP) + " / " + str(player_obj.MAX_HP)
+	$Vitals/VBoxContainer/HP_bar.max_value = player_obj.MAX_HP
+	$Vitals/VBoxContainer/HP_bar.value = player_obj.HP
+	$Vitals/VBoxContainer/HP_bar/Label.text = str(player_obj.HP) + " / " + str(player_obj.MAX_HP)
 
 func update_fuel():
-	$PanelContainer/VBoxContainer/Fuel_bar.max_value = player_obj.MAX_FUEL
-	$PanelContainer/VBoxContainer/Fuel_bar.value = player_obj.FUEL
-	$PanelContainer/VBoxContainer/Fuel_bar/Label.text = str(int(player_obj.FUEL)) + " / " + str(player_obj.MAX_FUEL)
+	$Vitals/VBoxContainer/Fuel_bar.max_value = player_obj.MAX_FUEL
+	$Vitals/VBoxContainer/Fuel_bar.value = player_obj.FUEL
+	$Vitals/VBoxContainer/Fuel_bar/Label.text = str(int(player_obj.FUEL)) + " / " + str(player_obj.MAX_FUEL)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
